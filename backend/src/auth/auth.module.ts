cffc7eba90312690cbd25b8535d7d9e59ec565cd
aleksakralj <64970957+aleksakralj@users.noreply.github.com>
2025-08-08 16:18:51 -0700

    Create auth.module.ts

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { AuthResolver } from './auth.resolver';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secretKey',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  providers: [AuthService, AuthResolver],
+  exports: [AuthService],
+})
+export class AuthModule {}
