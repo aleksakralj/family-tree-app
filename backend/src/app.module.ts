0069f0f9347c653baa92ba5a682feb404a96f952
aleksakralj <64970957+aleksakralj@users.noreply.github.com>
2025-08-08 16:13:01 -0700

    Create app.module.ts

---
+++ backend/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { PersonModule } from './person/person.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      context: ({ req }) => ({ req })
+    }),
+    PersonModule,
+    AuthModule,
+  ],
+})
+export class AppModule {}
